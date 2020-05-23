791c2e1ea8be7900d549c993f4ce7bb9d30170b5
Nilesh <nileshbhadana3@gmail.com>
2020-05-24 04:38:37 +0530

    Deploy Prometheus and Grafana in K8s cluster

---
+++ prometheus-grafana-deploy/README.md
@@ -0,0 +1,3 @@
+Run command 
+
+       > ./deploy.sh
---
+++ prometheus-grafana-deploy/deploy.sh
@@ -0,0 +1,16 @@
+sudo apt update -y
+sudo apt install nfs-kernel-server -y
+sudo cp exports /etc/exports
+kubectl config set-context --current --namespace default
+kubectl create -f class.yaml -f deployment.yaml -f rbac.yaml
+helm install stable/prometheus --name prometheus --values ./prometheus.values --namespace prometheus
+helm install stable/grafana --name grafana --values ./grafana.values --namespace grafana
+echo "Deploying... Please wait a minute"
+
+sleep 35
+
+kubectl get all -n prometheus
+kubectl get all -n grafana
+
+echo "Access grafana over <master-ip>:32323"
+echo "Access Prometheus over <master-ip>:32322"
